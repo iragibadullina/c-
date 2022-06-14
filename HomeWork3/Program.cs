a3a4b6c1fb1473a0ae29be41876cf2206f4e0f82
IrinaGibadullina <gibadullina.irina@me.com>
2022-06-14 22:16:39 +0500

    Homework3

---
+++ HomeWork3/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+
+// Задача № 3
+/*
+Console.Write("Input your number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void Cube(int num)
+{
+    int current = 1;
+    while(current <= num)
+    {
+        Console.Write((current * current * current) + " ");
+        current ++;
+    }
+}
+Cube(num);
+*/
+
