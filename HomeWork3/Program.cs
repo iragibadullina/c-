140b61650e28b1a0d9e7331eb3361d1bf54fec75
IrinaGibadullina <gibadullina.irina@me.com>
2022-06-15 17:09:32 +0500

    Homework is done

--- HomeWork3/Program.cs
+++ HomeWork3/Program.cs
@@ -1,19 +1,89 @@
-﻿/*
+﻿// Задача № 23. Вариант 1: Проверка вне метода.
 
-// Задача № 3
 /*
-Console.Write("Input your number: ");
-int num = Convert.ToInt32(Console.ReadLine());
-
 void Cube(int num)
 {
+    
     int current = 1;
     while(current <= num)
     {
         Console.Write((current * current * current) + " ");
-        current ++;
-    }
+        current ++ ;              
+    } 
+ 
+  
 }
+
+Console.Write("Input your number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num < 0) Console.WriteLine("Your number is negativ!");
+
 Cube(num);
 */
 
+// Задача № 23. Вариант 2: Проверка внутри тела метода.
+/*
+void Cube(int num)
+{
+    if(num > 0)
+    {
+        int current = 1;
+    while(current <= num)
+    {
+        Console.Write((current * current * current) + " ");
+        current ++ ;              
+    } 
+    }
+  else Console.WriteLine("Your number is negativ!");
+}
+
+Console.Write("Input your number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Cube(num);
+*/
+
+//Задача № 21.
+/*
+double FindLenght(double xA, double yA, double zA, double xB, double yB, double zB)
+{
+   return Math.Sqrt ((xB - xA)*(xB - xA) + Math.Pow((yB -yA),2) + Math.Pow((zB -zA),2));
+}
+ 
+double xA, yA, zA, xB, yB, zB, Distance;
+
+Console.WriteLine("Input coordinates xA: ");
+xA = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Input coordinates yA: ");
+yA = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Input coordinates zA: ");
+zA = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Input coordinates xB: ");
+xB = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Input coordinates yB: ");
+yB = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Input coordinates zB: ");
+zB = Convert.ToDouble(Console.ReadLine());
+
+Distance = FindLenght(xA, yA, zA, xB, yB, zB);
+Console.WriteLine("Вistance between points is " + Distance);
+*/
+
+// Задача № 19. Оставлю пока так. но доделаю
+void CheckingPolindromNumber(int num)
+{
+     if ((num / 10000 == num % 10) && ((num / 1000)%10 == (num % 100)/10)) Console.Write("Your number is polindrom");
+
+    else Console.Write("Your number is not polindrom") ;
+     }
+    
+  
+
+
+Console.Write("Input your number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+CheckingPolindromNumber(num);
