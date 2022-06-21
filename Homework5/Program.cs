18cff49c6cbe56e8189c6375f1053674fe35e830
IrinaGibadullina <gibadullina.irina@me.com>
2022-06-21 23:11:30 +0500

    Нужно разобраться

---
+++ Homework5/Program.cs
@@ -0,0 +1,61 @@
+﻿//Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+int[] CreateRandomArray(int size, int min, int max)
+{
+    int[] newArray = new int[size];
+    
+    for(int i = 0; i < size; i++)
+    {
+        newArray[i] = new Random().Next(min, max + 1);
+        Console.Write(newArray[i] + " ");
+    }
+    Console.WriteLine();
+
+    return newArray;
+}
+
+int CountPositive(int[] newArray)
+{
+    int count = 0;
+    for (int i = 0; i < newArray.Length; i++ )
+    {
+    if (array[i] % 2 == 1) 
+      count++;
+    }
+  
+  return count;
+}
+
+int min = 100;
+int max = 999;
+int result = CountPositive();
+Console.WriteLine("Sum of Positiv is " + CountPositiv());
+
+
+
+
+/*
+int FindCountPositivNumbers( int[] array, int min, int max){
+int count = 0;
+for( int i=0; i < array.Length; i++)
+{
+if( array[i] % 2 == 1 ) count++;
+}
+return count;
+}
+
+int minnum = 100;
+int maxnum = 999;
+int[] myArray = FindCountPositivNumbers;
+Console.Write("Count positiv numbers" + FindCountPositivNumbers(myArray, minnum, maxnum));
+
+
+//Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+*/
+
+
+//Задача 38: Задайте массив вещественных(дробных, см. Random().NextDouble()) чисел. Найдите разницу между максимальным и минимальным элементов массива.
+//[3 7 22 2 78] -> 76
---
+++ Seminar5/Program.cs
@@ -0,0 +1,125 @@
+﻿//Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. Найдите сумму отрицательных и положительных элементов массива.
+/*
+int[] CreateRandomArray(int size, int min, int max)
+{
+    int[] newArray = new int[size];
+    
+    for(int i = 0; i < size; i++)
+    {
+        newArray[i] = new Random().Next(min, max + 1);
+        Console.Write(newArray[i] + " ");
+    }
+    Console.WriteLine();
+
+    return newArray;
+} 
+
+int FindPositivSum(int[] array)
+{
+    int sum = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0) sum += array[i];
+    }
+    return sum;
+}
+
+int FindNegativSum(int[] array)
+{
+    int sum = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] < 0) sum += array[i]; // sum = sum + array[i]
+    }
+    return sum;
+}
+
+int[] myArray = CreateRandomArray(12, - 9, 9);
+Console.WriteLine("Sum of Positiv is " + FindPositivSum(myArray));
+Console.WriteLine("Sum of Negativ is " + FindNegativSum(myArray));
+*/
+
+//Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+
+int[] ChangeNegativOnPositiv(int[] array)
+{
+     for(int i = 0; i < array.Lehgth; i++)
+    {
+         array[i] * -1;
+      
+    }
+    return array;
+} 
+
+
+//Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+
+bool FingDigit (int[]array, int aa)
+{
+    bool bb = false;
+    for(i = 0; i < array.Length; i++)
+    {
+        if(array[i] == aa) bb = true;
+    }
+    return bb;
+
+}
+
+//Задайте одномерный массив из 12 случайных чисел. Найдите 
+
+int FindElements(int[] array, int min, int max)
+{
+    int count = 0;
+    for (int i = 0; i < array.Lehgth; i++)
+    {
+        if(array[i] >= min && array[i] <= max) count++;
+    }
+    return count;
+}
+
+
+
+
+bool FindNumberInArray(int[] array, int aa){
+for(int i = 0; i < array.Length; i++){
+if(array[i] == aa) return true;
+}
+return false;
+}
+
+int[] ReverseElementsArray(int[] array){
+for(int i = 0; i < array.Length; i++){
+array[i] *= -1;
+}
+return array;
+}
+
+int FindCountElemensInRange( int[] array, int min, int max){
+int count = 0;
+for( int i=0; i < array.Length; i++){
+if( array[i] >= min && array[i] <= max ) count++;
+}
+return count;
+}
+
+string OutputArrayConsole(int[] array){
+string ss = "";
+for(int i = 0; i < array.Length; i++){
+ss += array[i] + ", ";
+}
+return ss;
+}
+
+//программа
+Console.WriteLine("Работаем");
+int[] myArray = CreateRandomArray(20,-20,20);
+
+Console.WriteLine("reverse array " + OutputArrayConsole( ReverseElementsArray(myArray)) );
+int find = 16;
+Console.WriteLine("В массиве число "+ find + ( (FindNumberInArray(myArray,find)) ? "присутствует" : " не присутствует" ) );
+
+int minnum = 10;
+int maxnum = 99;
+Console.Write
+
+количество элементов массива, значения которых лежат в отрезке [10,99].
