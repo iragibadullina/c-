9242e287eb592a51b97ee7b910b002a3c87189cd
IrinaGibadullina <gibadullina.irina@me.com>
2022-06-24 08:22:21 +0500

    Массивы'

---
+++ Seminar6/Program.cs
@@ -0,0 +1,94 @@
+﻿/*
+int[] ReversArray(int[] array)
+
+
+{
+    int temp;
+    int j = array.Length - 1;
+
+    for(int i = 0; i < array.Length / 2; i++, j--)
+    {
+        temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
+*/
+/*
+while (i < j)
+{
+   temp = array[i];
+   array[i] = array[j];
+   array[j] = temp; 
+  
+   i++;
+   j--;
+}
+return array;
+*/
+/*
+int[] myArray = {3, 6, 9, 8, 5};
+myArray = ReversArray(myArray);
+for(int i = 0; i < myArray.Length; i++)
+{
+    Console.Write(myArray[i] + " ");
+}
+*/
+
+//Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
+//a + b > c
+/*
+bool Treangle(int numA, int numB, int numC)
+{
+    
+    if ((numA + numB > numC) && (numB + numC > numA) && (numC + numA > numB)) return true;
+  return false;
+  
+}
+ Console.WriteLine(Treangle(3, 3, 3)); 
+*/
+//Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+/*
+int[] ArrayFibonachi(int num)
+{
+    int[] array =  new int[num];
+    array[0] = 0;
+    array[1] = 1;
+    for(int i = 2; i < num; i++)
+    {
+        array[i] = array[i-2] + array[i-1];
+    }
+    return array;
+}
+
+string ArrayToString(int[] array)
+{
+    string[] arrayString = new string [array.Length];
+    string arrayToString = "";
+    for(int i = 0; i < array.Length; i++)
+    {
+        arrayString[i] = Convert.ToString(array [i]);
+        
+    }
+    arrayToString = string.Join(", ", arrayString);
+    return arrayToString;
+
+}
+
+Console.WriteLine(ArrayToString(ArrayFibonachi(4))); 
+*/
+//Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+string DecimalToDouble(int num)
+{
+    string result = string.Empty;
+    while(num > 0)
+    {
+        result = num % 2 + result;
+        num /= 2;
+    }
+    return result;
+}
+
+Console.WriteLine(DecimalToDouble(44));
