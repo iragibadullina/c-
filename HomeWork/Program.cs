3686b34e55d3da4e11093df81591cf94639b11ec
iragibadillina <gibadullina.irina@me.com>
2022-06-28 08:52:45 +0500

    commit

---
+++ HomeWork/Program.cs
@@ -0,0 +1,35 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+
+int CountPositiveNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++ )
+    {
+    if (array[i] > 0) 
+      count++;
+    }
+    return count;
+}
+Console.Write("Input your array: ");
+int[] myArray = Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
+Console.WriteLine("Count of Positiv Numbers is " + CountPositiveNumbers(myArray));
+
+
+
+//Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
+int[] CopyArray(int[] array)
+{
+    int[] newArray = new int[array.Length];
+    for(int i = 0; i < array.Length; i++)
+    {
+        newArray[i] = array[i];
+    }
+    return newArray;
+}
+Console.Write("Input your array: ");
+int[] myArray = Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
+Console.WriteLine("Copy Array: " + CopyArray(myArray));
---
+++ Semunar/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+
+int CountPositiveNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++ )
+    {
+    if (array[i] > 0) 
+      count++;
+    }
+    return count;
+}
+Console.Write("Input your array: ");
+int[] myArray = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+Console.WriteLine("Count of Positiv Numbers is " + CountPositiveNumbers(myArray));
+
+
+
